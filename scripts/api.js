1f0a8fb1893a0ad3a6cc002b4ae8a521ea0269bc
Jason Liszka <jliszka@alumni.cmu.edu>
2011-11-05 12:24:30 -0400

    oauth first attempt

---
+++ scripts/api.js
@@ -0,0 +1,19 @@
+function Foursquare(apiKey, authUrl, apiUrl) {
+  this.apiUrl = apiUrl;
+  if ($.bbq.getState('access_token')) {
+    // If there is a token in the state, consume it
+    this.token = $.bbq.getState('access_token');
+    $.bbq.pushState({}, 2)
+  } else if ($.bbq.getState('error')) {
+  } else {
+    this.doAuthRedirect(authUrl, apiKey, redirect);
+  }
+}
+
+Foursquare.prototype.doAuthRedirect = function(authUrl, apiKey) {
+  var redirect = window.location.href.replace(window.location.hash, '');
+  var url = authUrl + 'oauth2/authenticate?response_type=token&client_id=' + apiKey +
+    '&redirect_uri=' + encodeURIComponent(redirect) +
+    '&state=' + encodeURIComponent($.bbq.getState('req') || 'users/self');
+  window.location.href = url;
+};
